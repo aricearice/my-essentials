4e9b46d536dd0fd7b793b57212b39ae5f2248f7b
Alice Yang <aycy@me.com>
2016-01-18 20:54:50 -0500

    eslint

--- scripts/alice_vagrant.sh
+++ scripts/alice_vagrant.sh
@@ -6,10 +6,15 @@ sudo apt-get update && sudo apt-get install -y vim zsh tmux
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 chsh -s /bin/zsh # will need to enter password here
 # Put my dotfiles in place
-git clone https://github.com/aricearice/my-essentials.git ~/my-essentials
-cp -r ~/my-essentials/dotfiles/.vim* ~/
-cp -r ~/my-essentials/dotfiles/.zshrc ~/
-cp -r ~/my-essentials/dotfiles/.tmux.conf ~/
+cp -r $PWD/dotfiles/.vim* ~/
+cp -r $PWD/dotfiles/.zshrc ~/
+cp -r $PWD/dotfiles/.tmux.conf ~/
 # INstall vim plugins
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+# Install eslint
+sudo npm install -g eslint
+sudo npm install -g eslint-plugin-filenames
+
+# Configure git 
+git config --global core.editor vim
